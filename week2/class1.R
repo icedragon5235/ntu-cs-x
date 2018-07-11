2bdae9dd31272f58b7c708bf7601cd10317ff27a
icedragon5235 <40812682+icedragon5235@users.noreply.github.com>
2018-07-11 12:28:04 +0800

    777

--- week2/class1.R
+++ week2/class1.R
@@ -35,4 +35,15 @@ ggplot(iris1,aes(x=Sepal.Length,y=Sepal.Width,color=Species,size=Petalarea))+
   geom_point()
 ggplot(iris1,aes(x=Petal.Length,y=Petal.Width,color=Species,size=Sepalarea))+
   geom_point()
-
+#ggmap
+if(!require(ggmap))install.packages("ggmap",repos = "http://cran.us.r-project.org")
+library(ggmap)
+if(!require(mapproj))install.packages("mapproj",repos = "http://cran.us.r-project.org")
+if(!require(readr))install.packages("readr",repos = "http://cran.us.r-project.org")
+library(mapproj)
+library(readr)
+real<-read_csv('C:/Users/user/Desktop/codingsite/ntu-cs-x/week2/777.csv')
+map <- get_map(location =  c(lon = 120.2, lat = 23.7057668), zoom = 8,
+               language = "zh-TW")
+ggmap(map)+geom_point(aes(x=xwgs,y=ywgs,color='red'),data=real)+
+  ggtitle("台灣五十筆地址")
